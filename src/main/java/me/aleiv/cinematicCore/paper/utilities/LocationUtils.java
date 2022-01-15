9d548ea9b9d061d96bf2a122338a8ed450749809
DolphinGamerYT <dolphingamerytof@gmail.com>
2022-01-15 04:00:54 -0800

    Added new folder to .gitignore

--- src/main/java/me/aleiv/cinematicCore/paper/utilities/LocationUtils.java
+++ src/main/java/me/aleiv/cinematicCore/paper/utilities/LocationUtils.java
@@ -10,10 +10,11 @@ public class LocationUtils {
         for (int i = 0; i < 256; i++) {
             loc.add(0, -1, 0);
             if (loc.getBlock().getType().isSolid()) {
-                loc.add(0, 1, 0);
                 loc.setY(loc.getBlockY());
                 if (loc.getBlock().getType().toString().contains("SLAB") || loc.getBlock().getType().toString().contains("STAIRS")) {
                     loc.add(0, 0.5, 0);
+                } else {
+                    loc.add(0, 1, 0);
                 }
                 return loc;
             }
